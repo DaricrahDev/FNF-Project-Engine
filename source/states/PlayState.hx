804e945b87c7161e85b04c2846e9d39d9d165a42
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2023-11-05 12:38:18 -0300

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -60,7 +60,7 @@ import sys.io.File;
 #if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as VideoHandler;
 #elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as VideoHandler;
 #elseif (hxCodec == "2.6.0") import VideoHandler as VideoHandler;
-#end
+#else import vlc.VideoHandler; #end
 #end
 
 import objects.Note.EventNote;
@@ -775,6 +775,26 @@ class PlayState extends MusicBeatState
 			return;
 		}
 
+		var video:VideoHandler = new VideoHandler();
+			#if (hxCodec >= "3.0.0")
+			// Recent versions
+			video.play(filepath);
+			video.onEndReached.add(function()
+			{
+				video.dispose();
+				startAndEnd();
+				return;
+			}, true);
+			#else
+			// Older versions
+			video.playVideo(filepath);
+			video.finishCallback = function()
+			{
+				startAndEnd();
+				return;
+			}
+			#end
+		#else
 		FlxG.log.warn('Platform not supported!');
 		startAndEnd();
 		return;
